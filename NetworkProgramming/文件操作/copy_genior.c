313566d53fb621302b09e9dd5f0d45df011da94b
haomingt <2825493439@qq.com>
2026-04-22 09:56:06 -0400

    在copy_genior.c改善了copy.c,完美实现了复制文件功能

--- NetworkProgramming/文件操作/copy_genior.c
+++ NetworkProgramming/文件操作/copy_genior.c
@@ -14,21 +14,23 @@ int main(int argc,char *argv[]) {
     }
     printf("%s\n",argv[1]);
     printf("%s\n",argv[2]);
-    int copy_fd = open(argv[1],O_RDONLY);
+   int copy_fd = open(argv[1],O_RDONLY);
     if(copy_fd == -1)
         error_handling("copy open() error");
-    char buf[1024] = {0};
-    if(read(copy_fd,buf,sizeof(buf)) == -1)
-        error_handling("read() error");
-    close(copy_fd);
-    printf("copy file : %s\n",buf);
-    int patse_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,0644);
+    char buf[1];
+   int patse_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,0644);
     if(patse_fd == -1)
-        error_handling("patse open() error");
-    if(write(patse_fd,buf,sizeof(buf)) == -1){
+        error_handling("patse open() error"); 
+    int flag;
+    while((flag = read(copy_fd,buf,sizeof(buf))) != 0) {
+        if(flag == -1){
+            error_handling("read() error");
+        }
+       if(write(patse_fd,buf,sizeof(buf)) == -1){
         error_handling("write() error");
+       }
     }
+    close(copy_fd);
     close(patse_fd);
-    
     return 0;
-} 
+}
